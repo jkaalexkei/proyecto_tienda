126a221be862b1207b3585a578c6f89ebcc34360
Ing. Alexander Varela <jkaalexkei@gmail.com>
2021-08-26 22:42:13 -0500

    url con argumentos, respaldo,recuperacion de datos

--- proyecto_tienda/productos/urls.py
+++ proyecto_tienda/productos/urls.py
@@ -3,6 +3,8 @@ from django.urls.conf import path
 
 from . import views
 
+app_name = 'productos'
+
 urlpatterns = [
     #esta url recibe la llave primaria como parametro para la consulta del objeto
     path('buscar',views.ProductoBuscadorListView.as_view(),name='buscar'),#url del buscador
