dc98d7d29d135dcbbe42631572846c9a13d54ac1
Ing. Alexander Varela <jkaalexkei@gmail.com>
2021-09-05 23:28:36 -0500

    manejo de sesiones

--- proyecto_tienda/carrito/views.py
+++ proyecto_tienda/carrito/views.py
@@ -5,6 +5,14 @@ from django.shortcuts import render
 
 def carrito(request):
 
+    #el atributo sesion es una diccionario
+    #para crear una nueva sesion se debe crear una nueva clave con su respectiv0 valor
+    #la sesion se crea en el objeto request
+    request.session['carrito_id'] = '123'#aca creamos una nueva sesion
+
+
+
+
     return render(request,'carrito/carrito.html',{
 
     })
