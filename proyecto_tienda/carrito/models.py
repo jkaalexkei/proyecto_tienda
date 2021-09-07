44b803441e8b34c31455c01ac2e97f7655a84194
Ing. Alexander Varela <jkaalexkei@gmail.com>
2021-09-06 22:19:59 -0500

    trabajando con carritos de compras 1, creacion y consulta

--- proyecto_tienda/carrito/models.py
+++ proyecto_tienda/carrito/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.db.models.deletion import CASCADE
+from usuarios.models import User
+from productos.models import Producto
 # Create your models here.
+
+class Carrito(models.Model):
+    usuario =  models.ForeignKey(User,null=True,blank=True,on_delete=models.CASCADE)#relacion de uno a muchos con el modelo usuario
+    productos = models.ManyToManyField(Producto)#relacion muchos a muchos con el modelo productos
+    subtotal = models.DecimalField(default=0.0, max_digits=8,decimal_places=2)
+    total = models.DecimalField(default=0.0,max_digits=8,decimal_places=2)
+    created_ad = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return ''
--- proyecto_tienda/carrito/views.py
+++ proyecto_tienda/carrito/views.py
@@ -1,18 +1,54 @@
 from django.shortcuts import render
-
+from .models import Carrito
 # Create your views here.
 
 
 def carrito(request):
 
-    #el atributo sesion es una diccionario
-    #para crear una nueva sesion se debe crear una nueva clave con su respectiv0 valor
-    #la sesion se crea en el objeto request
-    request.session['carrito_id'] = '123'#aca creamos una nueva sesion
+    user = request.user if request.user.is_authenticated else None
 
+    carritoid = request.session.get('carritoid')
 
-
+    if carritoid:
+        carrito = Carrito.objects.get(pk=carritoid)#traemos el carrito de la BD
+    else:
+        carrito = Carrito.objects.create(usuario=user) #EN CASO QUE NO EXISTA SE CREA UN CARRITO
+    
+    request.session['carritoid'] = carrito.id
 
     return render(request,'carrito/carrito.html',{
 
     })
+
+    #el atributo sesion es un diccionario
+    #para crear una nueva sesion se debe crear una nueva clave con su respectiv0 valor
+    #la sesion se crea en el objeto request
+
+    #crear una sesion
+    #request.session['carrito_id'] = '123'#aca creamos una nueva sesion
+                   #nombre de la sesion = valor de la sesion
+    
+    #consultar una sesion
+    # valorsesion = request.session.get('carrito_id')
+    # print(valorsesion)
+
+    # #Eliminar un sesion
+    # request.session['carrito_id'] = None #para eliminar se le asigna a la sesion el valor de None
+    # print(valorsesion)
+
+    #comprobacion de carrito de comprar utilizando las sesiones
+    # user = request.user if request.user.is_authenticated else None
+
+    # carrito_id = request.session.get('carrito_id')
+
+    # if carrito_id:
+    #     #obtenemos el carrito de la base de datos
+    #     carrito = Carrito.objects.get(pk=carrito_id)
+        
+    # else:#se crea un nuevo carrito
+    #     carrito = Carrito.objects.create(usuario = user)
+        
+    # request.session['carrito_id'] = carrito.id
+
+
+    
